0ba6cba96e1b6d36801ed64f59085277fba4b951
Elijah Kleeman <contact@elijahk.com>
2020-06-05 18:51:23 -0700

    Add logging to file as part of normal logger

--- src/utility/logging.cpp
+++ src/utility/logging.cpp
@@ -2,16 +2,23 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+#include <string>
 
 namespace utility
 {
+
+const std::string logging_file_path{"logs/log.txt"};
 
 spdlog::logger create_logger()
 {
     auto loggingSinks = create_platform_sinks();
     auto stdoutSink   = std::make_shared<spdlog::sinks::stdout_sink_mt>();
+    auto fileSink     = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logging_file_path, true);
 
     loggingSinks.push_back(stdoutSink);
+    loggingSinks.push_back(fileSink);
 
     return spdlog::logger("logger", loggingSinks.begin(), loggingSinks.end());
 }
