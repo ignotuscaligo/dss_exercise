0dd78aa89cf5b613dfc41b98d30dfcef89d9900c
Elijah Kleeman <contact@elijahk.com>
2020-06-06 18:15:45 -0700

    Add json checking inside `Game`
    - If the necessary fields are missing for getting nice headlines and images, fallback to a basic "<Home team> vs <Away team>" headline.

--- src/mlb/Game.cpp
+++ src/mlb/Game.cpp
@@ -8,14 +8,38 @@ namespace mlb
 Game::Game(web::json::value object)
 {
     auto content = object[U("content")];
-    auto mlbRecap = content[U("editorial")][U("recap")][U("mlb")];
-    auto imageCut = mlbRecap[U("photo")][U("cuts")][U("248x138")];
 
-    m_headline    = utility::as_std_string(mlbRecap[U("headline")].as_string());
-    m_subHeadline = utility::as_std_string(mlbRecap[U("subhead")].as_string());
-    m_imageUrl    = utility::as_std_string(imageCut[U("src")].as_string());
-    m_imageWidth  = utility::as_std_string(imageCut[U("width")].as_integer());
-    m_imageHeight = utility::as_std_string(imageCut[U("height")].as_integer());
+    if (content.has_field(U("editorial")))
+    {
+        auto editorial = content[U("editorial")];
+
+        if (editorial.has_field(U("recap")))
+        {
+            auto recap = editorial[U("recap")];
+
+            if (recap.has_field(U("mlb")))
+            {
+                auto mlbRecap = recap[U("mlb")];
+
+                m_headline    = utility::as_std_string(mlbRecap[U("headline")].as_string());
+                m_subHeadline = utility::as_std_string(mlbRecap[U("subhead")].as_string());
+
+                auto imageCut = mlbRecap[U("photo")][U("cuts")][U("248x138")];
+
+                m_imageUrl    = utility::as_std_string(imageCut[U("src")].as_string());
+                m_imageWidth  = imageCut[U("width")].as_integer();
+                m_imageHeight = imageCut[U("height")].as_integer();
+            }
+        }
+    }
+
+    if (m_headline.empty())
+    {
+        auto homeTeam = utility::as_std_string(object[U("teams")][U("home")][U("team")][U("name")].as_string());
+        auto awayTeam = utility::as_std_string(object[U("teams")][U("away")][U("team")][U("name")].as_string());
+
+        m_headline = homeTeam + " vs. " + awayTeam;
+    }
 }
 
 std::string Game::headline() const
